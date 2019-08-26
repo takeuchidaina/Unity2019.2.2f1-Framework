b15b9a5a82c8ef667728d7324933a0289178e93d
takeuchidaina <yda.takeuchidaina@gmail.com>
2019-08-27 01:53:52 +0900

    [add]フレームワークとなるUnityのシーンをまるごと追加

---
+++ Framework/Assets/Script/SceneChange.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;  //シーン管理できるようにするやつ
+using UnityEngine;
+
+public class SceneChange : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ToTitleScene()
+    {
+        SceneManager.LoadScene("Title");
+    }
+    public void ToInGameScene()
+    {
+        SceneManager.LoadScene("InGame");
+    }
+    public void ToResultScene()
+    {
+        SceneManager.LoadScene("Result");
+    }
+    public void ToGameEnd()
+    {
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #elif UNITY_STANDALONE
+            UnityEngine.Application.Quit();
+        #endif
+    }
+}
---
+++ Framework/Assets/Script/SetFPS.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetFPS : MonoBehaviour
+{
+    void Awake()
+    {
+        Application.targetFrameRate = 60; //60FPSに設定
+
+        /*
+         * プロジェクトの設定でFPS値の垂直同期を切っておかないとだめ
+         * 確認方法
+         * "Edit"→"Project Settings"→"Quality"を選んで、インスペクターから"VSync Count"
+         * ドロップダウンメニューから"Don't Sync"（垂直同期無し）を選ぶ。
+         * 参考URL：http://unityleaning.blog.fc2.com/blog-entry-2.html
+         */
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
